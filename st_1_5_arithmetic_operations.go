b251d7a81622175225dc142dce149b67184c2d49
Helen <rotach.elena@gmail.com>
2021-07-15 20:06:11 +0300

    Арифметические операции

--- st_1_5_arithmetic_operations.go
+++ st_1_5_arithmetic_operations.go
@@ -1,5 +1,29 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	a := 100
+	b := 10
+
+	c := a + b // с = 110
+	fmt.Println(c)
+
+	c = a * b  // с = 1000
+	fmt.Println(c)
+
+	c = a - b// с = 90
+	fmt.Println(c)
+
+	c = a / b  // с = 10
+	fmt.Println(c)
+
+	c = a % b  // с = 0
+	fmt.Println(c)
+
+	c++  // с = 1
+	fmt.Println(c)
+
+	c--  // с = 0
+	fmt.Println(c)
 }
