90f0e35022d4d434ea16fc8e82291013beaf1803
Helen <rotach.elena@gmail.com>
2021-07-15 20:30:42 +0300

    Ввод/вывод

--- st_1_5_fmt.go
+++ st_1_5_fmt.go
@@ -11,6 +11,7 @@ func main() {
 	fmt.Scan(&age)
 
 	fmt.Println(name, age)
+	fmt.Println("=================================================")
 
 	// чтение нескольких переменных
 	var (
@@ -21,4 +22,18 @@ func main() {
 	fmt.Scan(&a, &b, &c)
 
 	fmt.Println(a, b, c)
+	fmt.Println("=================================================")
+
+	// вывод в одну/несколько строк (если не строки, то разделяется пробелом)
+	fmt.Print("test", 1) // test1
+	fmt.Println("")
+	fmt.Println("test", 2) // test 2
+	fmt.Print(3, 4) // 3 4
+	fmt.Println("")
+	fmt.Println("=================================================")
+
+	myName := "Helen"
+	myAge := 34
+
+	fmt.Println("My name is", myName, "and I am", myAge, "years old.")
 }
