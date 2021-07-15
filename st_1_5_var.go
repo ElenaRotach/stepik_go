3de1a017b26fdab70506a534345ddd55ddde61a9
Елена Ротач <e.rotach@tektorg.ru>
2021-07-15 19:54:14 +0300

    Объявление нескольких переменных

--- st_1_5_var.go
+++ st_1_5_var.go
@@ -1,5 +1,5 @@
 package main
-//go run st_1_var.go
+//go run st_1_5_var.go
 import "fmt"
 
 func main() {
@@ -7,10 +7,15 @@ func main() {
 	var hello string
 	var a int = 2019
 	var symbol int32 = 'c'
-
+	var (
+		name string = "Helen"
+		age int = 34
+	)
 	hello = "Hello Go!"
 
 	fmt.Println(hello)
 	fmt.Println(a)
 	fmt.Println(string(symbol))
+	fmt.Println(name)
+	fmt.Println(age)
 }
