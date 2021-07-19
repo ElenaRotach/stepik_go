3bbcfbce8d436c03c441e289de2e97579c94fceb
Helen <rotach.elena@gmail.com>
2021-07-19 15:02:18 +0300

    Условия

---
+++ st_1_8_if_else.go
@@ -0,0 +1,24 @@
+//go run st_1_8_if_else.go
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	a := 6
+	b := 7
+	if a < b {
+		fmt.Println("a меньше, чем b")
+	}
+
+	// краткая запись
+	lim := 10.0
+	x := 2
+	n := 3
+	if v := math.Pow(float64(x), float64(n)); v < lim {
+		// v видима только в if
+		fmt.Println(v)
+	}
+}
